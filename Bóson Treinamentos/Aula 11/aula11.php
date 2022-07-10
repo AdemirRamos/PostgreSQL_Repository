9253bdfabcf39bc98c2c82adc4b25df38158efbc
AdemirRamos <ademirramos634@gmail.com>
2022-07-10 12:32:03 -0300

    Adição da Aula 11
    Neste 'commit' foi adicionada a aula 11 (sobre tipos de dados).

---
+++ Bóson Treinamentos/Aula 11/aula11.php
@@ -0,0 +1,19 @@
+<?php
+
+	/*
+
+		Aula 11 - Tipos de Dados
+
+		Tipos de dados em PostgreSQL:
+
+		O PostgreSQL possui uma rica variedade de tipos de dados que podem ser empregados na definição de colunas em tabelas.
+
+		Esses tipos englobam os domínios numérico, texto, data e hora, geométrico, "booleano", endereçamento de redes, enumeração, e até mesmo tipos definidos pelo usuário, com o uso de declaração "CREATE TYPE".
+
+		Veremos, a seguir, alguns dos tipos mais comuns usados em postgres.
+
+		P. S.: Para maior riqueza de detalhes, assistir a aula 11.
+
+	*/
+
+?>
