f1d7d5a75302f152657eac75bd9d8cd4750d7f36
AdemirRamos <ademirramos634@gmail.com>
2022-07-10 11:59:29 -0300

    Adição da Aula 09
    Neste 'commit' foi adicionado o arquivo 'aula09.php' com suas anotações sobre como usar o pgAdmin 4.

---
+++ Bóson Treinamentos/Aula 09/aula09.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+
+	Aula 09 - Como Instalar eAacessar o "pgAdmin4" No Linux Debian
+
+	Nessa aula, é ensinado como criar um servidor através do pgAdmin 4.
+
+	Para criar um novo banco de dados, basta selecionar a opção "database" e, em seguida, selecionar a opção "create" e "database".
+
+	Ao criar um banco de dados, na aba "SQL" há informações sobre o código SQL usado para criar o "database" em questão.
+
+	O valor "-1" significa "infinito".
+
+	Para encerrar o servidor do pgAdmin 4, é preciso selecionar o ícone do PostgreSQL e clicar em "shut down server".
+
+*/
+
+?>
