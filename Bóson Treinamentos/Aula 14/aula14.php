983b74fc0b898397c8cf93e849d610ef008b5beb
AdemirRamos <ademirramos634@gmail.com>
2022-07-12 23:51:49 -0300

    Adição da Aula 14
    Neste 'cpmmit' foi adicionado o arquivo 'aula14.php' junto de suas anotações sobre o comando 'SELECT'.

---
+++ Bóson Treinamentos/Aula 14/aula14.php
@@ -0,0 +1,22 @@
+<?php
+
+	/*
+
+		Aula 14 - Como Realizar Consultas Simples Em Tabelas Com o Comando "SELECT"
+
+		SELECT * FROM clientes;
+		SELECT nome_cliente FROM clientes;
+		SELECT nome_cliente, sobrenome FROM clientes;
+		SELECT * FROM produtos;
+		SELECT nome_produto FROM produtos;
+		SELECT nome_produto, description FROM produtos;
+		SELECT nome_produto, description, price FROM produtos;
+
+		-- Também é possível separar o "SELECT" do "FROM" através da quebra de linha (lembre-se de usar ponto e vírgula) ao final do código.
+
+		SELECT *
+		FROM pedidos;
+
+	*/
+
+?>
