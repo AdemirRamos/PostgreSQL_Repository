087b25857bbb53e79261c1c4e67ade458979b894
AdemirRamos <ademirramos634@gmail.com>
2022-07-13 00:19:18 -0300

    Atualização da Aula 15
    As anotações que compõem o arquivo foram adicionadas a ele.

--- Bóson Treinamentos/Aula 15/aula15.php
+++ Bóson Treinamentos/Aula 15/aula15.php
@@ -4,7 +4,23 @@
 
 		Aula 15 - Como Filtrar Consultas Com a Cláusula "WHERE" No PostgreSQL
 
-		
+		SELECT * FROM clientes;
+
+		SELECT * FROM clientes
+		WHERE código_cliente = 1;
+
+		-- Também é possível usar operadores relacionais/de comparação em SQL:
+
+		SELECT nome_produto, quantidade_estoque FROM produtos
+		WHERE quantidade_estoque < 10;
+
+		SELECT nome_produto, price FROM produtos
+		WHERE price >= 15.00;
+
+		SELECT código_produto, quantidade_adquirida FROM pedidos
+		WHERE código_cliente = 2;
+
+		SELECT código_cliente FROM pedidos;
 
 	*/
 
