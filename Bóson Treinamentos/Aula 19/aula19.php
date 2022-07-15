f0c446f6269381a30598f53add14847168a9b628
AdemirRamos <ademirramos634@gmail.com>
2022-07-14 22:32:39 -0300

    Adição da Aula 19
    Neste 'commit' foi adicionada a aula 19 sobre o operador lógico 'BETWEEN'.

---
+++ Bóson Treinamentos/Aula 19/aula19.php
@@ -0,0 +1,23 @@
+<?php
+
+	/*
+
+		Aula 19 - Operador "BETWEEN" e Intervalos Em Consultas SQL
+
+		"BETWEEN" é um operador de comparação.
+
+		Esse operador permite a visualização (nos resultados das consultas) filtros de intervalos de dados.
+
+		Sintaxe:
+
+		SELECT ...
+		FROM ...
+		WHERE colune (s) (NOT) BETWEEN valor1 AND valor2
+
+		"NOT BETWEEN" serve para buscar dados fora de um intervalo específico.
+
+		
+
+	*/
+
+?>
