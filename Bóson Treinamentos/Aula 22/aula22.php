bc9bf1e35958cb3eb435d2462ba7c6e7e2f233ea
AdemirRamos <ademirramos634@gmail.com>
2022-07-16 12:52:55 -0300

    Atualização da Aula 22

---
+++ Bóson Treinamentos/Aula 22/aula22.php
@@ -0,0 +1,27 @@
+<?php
+
+	/*
+
+		//Aula 22 - 10 Exemplos de Funções de Agregação No PostgreSQL
+
+		As funções de agregação do PostgreSQL:
+
+		São usadas para computar um valor único a partir de um conjunto de valores de entrada.
+
+		Funções Básicas de Agregação:
+
+		1) COUNT: faz contagem de itens;
+		2) MAX: encontra o valor máximo;
+		3) MIN: encontra o valor mínimo;
+		4) AVG: encontra o valor médio;
+		5) SUM: retorna o somatório dos valores apresentados.
+		
+		
+
+		Anotações Extras:
+
+		Fatal Error when opening pgadmin4: https://community.progress.com/s/article/fatal-error-when-opening-pgadmin4
+
+	*/
+
+?>
